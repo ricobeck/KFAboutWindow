10b4140ba086aa96f611c9cb24e3fbd2e2f94fb6
rico becker <rico.becker@kf-interactive.com>
2014-03-22 00:19:36 +0100

    Adds iOS project

---
+++ Classes/ios/KFAboutViewController.m
@@ -0,0 +1,74 @@
+//
+//  KFAboutViewController.m
+//  AboutScreenDemo
+//
+//  Created by Gunnar Herzog on 21/03/14.
+//  Copyright (c) 2014 KF Interactive. All rights reserved.
+//
+
+#import "KFAboutViewController.h"
+
+@interface KFAboutViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (strong, nonatomic) UIImageView *titleImageView;
+
+@property (strong, nonatomic) UIView *titleView;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewWidthConstraint;
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+
+@end
+
+@implementation KFAboutViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.imageView.image = [UIImage imageNamed: [[[[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIcons"] objectForKey:@"CFBundlePrimaryIcon"] objectForKey:@"CFBundleIconFiles"] firstObject]];
+    self.titleImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.imageViewWidthConstraint.constant, self.imageViewWidthConstraint.constant)];
+    self.titleImageView.image = self.imageView.image;
+
+    self.titleView = [UIView new];
+    self.titleView.layer.masksToBounds = YES;
+    [self.titleView addSubview:self.titleImageView];
+
+    self.navigationItem.titleView = self.titleView;
+    
+    NSURL *creditsURL = [[NSBundle mainBundle] URLForResource:@"Credits" withExtension:@"rtf"];
+    self.textView.attributedText = [[NSAttributedString alloc] initWithFileURL:creditsURL options:nil documentAttributes:nil error:nil];
+}
+
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    self.titleView.frame = CGRectMake(0, 0, 320, 44);
+
+    self.titleImageView.frame = CGRectMake(110, 64, self.imageViewWidthConstraint.constant, self.imageViewWidthConstraint.constant);
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat a = scrollView.contentOffset.y;
+    CGFloat a0 = -64;
+    CGFloat a1 = 5;
+    CGFloat b0 = 100;
+    CGFloat b1 = 34;
+
+    CGFloat b = ((a - a0) * (b1 - b0)) / (a1 - a0) + b0;
+    b = MAX(MIN(b, b0), b1);
+    self.imageViewWidthConstraint.constant = b;
+
+    CGFloat x = (CGRectGetWidth([UIScreen mainScreen].applicationFrame) - b) / 2.0;
+    x = floor([self.titleView convertPoint:CGPointMake(x, 0) fromView:nil].x);
+    self.titleImageView.frame = CGRectMake(x, MAX(a1, -a), b, b);
+}
+
+@end
